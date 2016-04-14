00410c92363552e8da2f42e6f52fb8547532f006
Andrew Badr <andrewbadr+git@gmail.com>
2016-04-14 18:33:34 -0400

    remove photoswipe animations

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 
         if(index >= 0) {
             // open PhotoSwipe if valid index found
-            openPhotoSwipe( index, clickedGallery );
+            openPhotoSwipe( index, clickedGallery, true );
         }
         return false;
     };
@@ -149,8 +149,8 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
                     rect = thumbnail.getBoundingClientRect(); 
 
                 return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
-            }
-
+            },
+            hideAnimationDuration: 0
         };
 
         // PhotoSwipe opened from URL
